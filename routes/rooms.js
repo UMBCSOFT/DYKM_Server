70d23c6522f0babb4cc83b7fbb3e6884ae034900
Toocanzs <dupanzszs@gmail.com>
2021-04-07 10:33:42 -0400

    Change routes to /rooms/*, so /room/get and /room/create to keep things consistent

--- routes/rooms.js
+++ routes/rooms.js
@@ -10,7 +10,7 @@ rooms = new Map()
 
 // put all routes in this function! these will be automatically registered.
 module.exports = function(app){
-    app.get('/get/room/:id', (req, res) => {
+    app.get('/room/get/:id', (req, res) => {
         const room = rooms[req.params.id];
         if(room === undefined) {
             res.status(500).send(`No such room exists with id ${req.params.id}`);
@@ -19,7 +19,7 @@ module.exports = function(app){
         res.send(room);
     });
 
-    app.post('/create/room', (req, res) => {
+    app.post('/room/create', (req, res) => {
         const id = uuidv4();
         let room = new Room(id);
         rooms[id] = room;
