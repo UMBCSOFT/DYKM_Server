0fb297f3b9cc7c378ae82f2cd9d3a3275019f5c1
Toocanzs <dupanzszs@gmail.com>
2021-04-07 10:32:08 -0400

    Use uuid as room id

--- routes/rooms.js
+++ routes/rooms.js
@@ -6,19 +6,23 @@ class Room {
     }
 }
 
-rooms = []
+rooms = new Map()
 
 // put all routes in this function! these will be automatically registered.
 module.exports = function(app){
     app.get('/get/room/:id', (req, res) => {
-        const room = rooms.find(c => c.id === parseInt(req.params.id));
+        const room = rooms[req.params.id];
+        if(room === undefined) {
+            res.status(500).send(`No such room exists with id ${req.params.id}`);
+            return;
+        }
         res.send(room);
     });
 
     app.post('/create/room', (req, res) => {
         const id = uuidv4();
         let room = new Room(id);
-        rooms.push(room);
+        rooms[id] = room;
         res.send(room);
     });
 }
