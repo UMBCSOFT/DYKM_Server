7d1197e01bb14eb77de760736f36f135dfbec6c4
Steven laczko <slaczko1@umbc.edu>
2021-04-11 13:50:17 -0400

    Enums for states and message types
    Some not-working WS code in index.js
    
    Installed ws

--- index.js
+++ index.js
@@ -2,10 +2,28 @@ const express = require('express');
 const settings = require('./settings')
 const app = express();
 const PORT = process.env.PORT || 1337;
+const http = require('http');
+const WebSocket = require('ws');
+
+// create http server and websocket
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server })
 
 // get all routes in /routes and register them
 require('./routes/routes_index')(app);
 
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+
+    //log the message and send back (little test)
+    console.log(`received: ${message}`);
+    ws.send(`Hello, you sent -> ${message}`);
+  });
+
+  ws.send('Hi there. This is a WS server.');
+});
+
 app.listen(PORT, () => {
   console.log(`Listening at http://localhost:${PORT}`);
 })
+
