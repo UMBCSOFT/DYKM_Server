35851f5b3b2bb965b943b8673329702b5a6837d9
Steven laczko <slaczko1@umbc.edu>
2021-04-14 19:39:25 -0400

    Attempted to move the JOIN message code somewhere besides index.js. It just... ain't right, but I'm not sure how to fix it.

--- index.js
+++ index.js
@@ -4,19 +4,16 @@ const app = express();
 const http = require('http');
 const WebSocket = require('ws');
 const path = require('path');
-const Player = require('./player');
-const Connection = require('./connection');
+const rooms = require('routes/rooms');
 
 app.heartbeatInterval = 15000; // 15 seconds
 
-function removeItemOnce(arr, value) {
-  var index = arr.indexOf(value);
-  if (index > -1) {
-    arr.splice(index, 1);
+//
+function updateRooms(app) {
+  for(let room of app.rooms.values()) {
+    room.update(app, settings.updateInterval);
   }
-  return arr;
 }
-
 
 // create http server and websocket
 const server = http.createServer(app);
@@ -25,47 +22,12 @@ const wss = new WebSocket.Server({ port: settings.WebsocketPort })
 // get all routes in /routes and register them
 require('./routes/routes_index')(app);
 
-app.rooms = new Map();
 // This is where websocket connections remain until they send a join room message.
 // We need this because there's no way to connect to a single room without sending that message of the websocket, which means
 // there is an intermediate state where you're connected to the websocket server but haven't joined a room yet
 let connectedButNotInRoom = [];
-
-wss.on('connection', (ws) => {
-  let connection = new Connection(ws);
-  connectedButNotInRoom.push(connection);
-  ws.on('message', (message) => {
-    if(message === "PONG")
-    {
-      ws.send("PONG ACK");
-      console.log("Replying to PONG");
-      return;
-    }
-
-    if(connection.joinedRoom){
-      // Push onto queue for the room update function to read later
-      connection.messages.push(message);
-    }
-    // Since we're not in a room yet we handle JOIN here, but non JOIN messages should be handled in Room.update()
-    else if(message.startsWith('JOIN ')) {
-      let roomCode = message.substr("JOIN ".length);
-      message.substr()
-      let room = app.rooms.get(roomCode);
-      if(room !== undefined) {
-        // Remove connection from the unconnected state, and add it as a new player to the room
-        removeItemOnce(connectedButNotInRoom,connection);
-        let player = new Player("Player" + (room.players.length + 1));
-        player.connection = connection;
-        room.players.push(player);
-        player.connection.joinedRoom = true;
-        ws.send("WELCOME " + player.nickname);
-      }
-    }
-    else {
-      throw new Error(`Haven't joined room a yet but received a non JOIN message "${message}"\nA join message must be sent before sending other websocket messages`)
-    }
-  });
-});
+app.rooms = new Map();
+rooms
 
 app.listen(process.env.PORT || settings.PORT, () => {
   console.log(`Listening at http://localhost:${process.env.PORT || settings.PORT}`);
@@ -76,10 +38,12 @@ app.get('/', function(req, res){
   res.sendFile( path.join(__dirname + '/test_platform/index.html'));
 });
 
-function updateRooms(app) {
-  for(let room of app.rooms.values()) {
-    room.update(app, settings.updateInterval);
-  }
-}
-
 setInterval(updateRooms, settings.updateInterval, app);
+
+// export all functions for unit testing
+module.exports = {
+  updateRooms: updateRooms,
+  server: server,
+  wss: wss
+};
+
--- routes/rooms.js
+++ routes/rooms.js
@@ -1,9 +1,12 @@
 const { v4: uuidv4 } = require('uuid');
+const Player = require('../player');
+const Connection = require('../connection');
 
 const Room = require('../room');
 
 // put all routes in this function! these will be automatically registered.
 module.exports = function(app){
+    // Request to connect to a server room with :id id
     app.get('/room/get/:id', (req, res) => {
         const room = app.rooms.get(req.params.id);
         if(room === undefined) {
@@ -14,11 +17,58 @@ module.exports = function(app){
         res.send("ws://localhost:4567"); // TODO: Once released this needs to return the IP of our live server
     });
 
+    // Request to create a new server room (as the host, of course)
     app.post('/room/create', (req, res) => {
         const id = uuidv4();
-        let room = new Room(id);
+        let room = new Room(id, States.GAME_READY_HOST);
         app.rooms.set(id, room);
         console.log(`Created room: ${id} - ${app.rooms.size}`);
         res.send(room);
     });
+
+    function removeItemOnce(arr, value) {
+        let index = arr.indexOf(value);
+        if (index > -1) {
+            arr.splice(index, 1);
+        }
+        return arr;
+    }
+
+    function Join(wss, connectedButNotInRoom) {
+        wss.on('connection', (ws) => {
+            let connection = new Connection(ws);
+            connectedButNotInRoom.push(connection);
+            ws.on('message', (message) => {
+                if(message === "PONG")
+                {
+                    ws.send("PONG ACK");
+                    console.log("Replying to PONG");
+                    return;
+                }
+
+                if(connection.joinedRoom){
+                    // Push onto queue for the room update function to read later
+                    connection.messages.push(message);
+                }
+                // Since we're not in a room yet we handle JOIN here, but non JOIN messages should be handled in Room.update()
+                else if(message.startsWith('JOIN ')) {
+                    let roomCode = message.substr("JOIN ".length);
+                    message.substr()
+                    let room = app.rooms.get(roomCode);
+                    if(room !== undefined) {
+                        // Remove connection from the unconnected state, and add it as a new player to the room
+                        removeItemOnce(connectedButNotInRoom,connection);
+                        let player = new Player("Player" + (room.players.length + 1));
+                        player.connection = connection;
+                        room.players.push(player);
+                        player.connection.joinedRoom = true;
+                        ws.send("WELCOME " + player.nickname);
+                    }
+                }
+                else {
+                    throw new Error(`Haven't joined room a yet but received a non JOIN message "${message}"\nA join message must be sent before sending other websocket messages`)
+                }
+            });
+        });
+    }
 }
